43de8913376d86caefca709b9d5efc86242df84a
EriveltoSilva <eriveltoclenio@gmail.com>
2024-06-25 01:45:13 +0100

    first commit

---
+++ src/test/java/com/eventosangola/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventosangola.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
